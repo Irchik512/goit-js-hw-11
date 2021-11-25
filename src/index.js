05a2ea49fb7468b89ace06f5d44cfe66e4110adb
Irchik512 <chornairchik@gmail.com>
2021-11-25 22:08:13 +0200

    remove to mac

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import axios from 'axios';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  /* options */  refresh()
+});
